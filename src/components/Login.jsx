e4b39ba6b4ddc53db3af0ca4168d8973e1c07397
Azhar Abbas <mirzaazharabbas10@gmail.com>
2025-08-18 17:52:32 +0500

    Added Code for Login and Register Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [formDate, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  async function handleLogin(e) {
+    
+    console.log(formDate);
+    e.preventDefault();
+    const res = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify(formDate),
+    });
+
+    const data = await res.json();
+    if (data.code === 422) {
+      setErrors(data.message);
+    } else if (data.code === 404) {
+      setErrors({ general: data.message });
+    } else {
+      console.log("Other error:", data);
+    }
+  }
+  useEffect(() => {
+    if (errors.general) {
+      const timer = setTimeout(() => {
+        setErrors({});
+      }, 3000);
+      return () => clearTimeout(timer); // cleanup
+    }
+  }, [errors]);
+
+   function handleChange(field, value) {
+    setFormData({ ...formDate, [field]: value });
+
+    if (errors[field]) {
+      setErrors((prev) => ({ ...prev, [field]: "" }));
+    }
+  }
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
@@ -12,22 +53,37 @@ export default function Login() {
           Please login to continue
         </p>
 
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleLogin} className="flex flex-col gap-4">
+          {errors.general && (
+            <div className="bg-red-100 text-red-700 px-4 py-2 rounded mb-4">
+              {errors.general}
+            </div>
+          )}
           <div className="flex flex-col">
             <label className="mb-1 font-medium text-gray-700">Email</label>
             <input
-              type="email"
+              type="text"
               placeholder="Enter your email"
-              className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={formDate.email}
+              onChange={(e) => handleChange("email", e.target.value)}
+              className="px-4 py-2 border border-gray-300 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email[0]}</p>
+            )}
           </div>
           <div className="flex flex-col">
             <label className="mb-1 font-medium text-gray-700">Password</label>
             <input
               type="password"
               placeholder="Enter your password"
-              className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={formDate.password}
+              onChange={(e) => handleChange("password", e.target.value)}
+              className="px-4 py-2 border border-gray-300 text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password[0]}</p>
+            )}
           </div>
 
           <button
